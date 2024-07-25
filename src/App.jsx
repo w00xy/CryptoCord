628ef1001abaacbe0878bc3698d02dbbb152e15f
w00xy <vlad42006@mail.ru>
2024-07-25 08:51:26 +0300

    Added adaptive layout and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Coin from './pages/Coin/Coin'
+import Footer from './components/footer/Footer'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/coin/:coinId' element={<Coin/>}/>
       </Routes>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <p>Copyright @ 2024, CryptoCord - All Right Reserved.</p>
+    </div>
+  )
+}
